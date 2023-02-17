139cf6d100b7b935b9343e6a9f11e7b302154c18
Marianadaso3 <72479030+Marianadaso3@users.noreply.github.com>
2023-02-16 23:59:25 -0600

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from BayesModel import graficar 
+from BayesModel import descripcionCompleta 
+from BayesModel import lista 
+from BayesModel import modelo 
+from BayesModel import factores
+# 'metodo' es una función que se encuentra en un archivo llamado 'archivo.py'
